b8008ab4db5028c58ed8f372435a6bcf7d515a34
chinhan99 <braydenyong@yahoo.com.sg>
2022-07-09 02:04:18 +0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Counter from "./Counter.svelte";
+  let message = "hi";
+</script>
+
+<div>{message}</div>
+<input id="message" type="text" bind:value={message} /><br />
+<button on:click={() => (message = "bye")}>say bye</button>
+
+<div>
+  <Counter />
+</div>
